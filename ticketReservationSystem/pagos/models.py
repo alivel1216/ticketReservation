6e3cc3e2befdd8b7e197c2dd4d28825a0e7c5b57
alivel1216 <70455689+alivel1216@users.noreply.github.com>
2020-09-14 00:36:24 -0500

    pagos

--- ticketReservationSystem/pagos/models.py
+++ ticketReservationSystem/pagos/models.py
@@ -1,3 +1,40 @@
-from django.db import models
 
+from django.db import models
+from django import forms
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+from reservas.models import Reservar
+
+TYPE_PAY = [
+    ('transferencia', 'Transeferencia Bancaria'),
+    ('tarjeta', 'Tarjeta de Credito'),
+]
 # Create your models here.
+
+class Pagar(models.Model):
+    
+    reservacion = models.OneToOneField(Reservar, on_delete=models.CASCADE)
+    tipo_pago = models.CharField(max_length=30)
+    name_prop = models.CharField(max_length=30)
+    num_card = models.CharField(max_length=19)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name_prop
+
+class cliente(models.Model):
+    usuario = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+
+class PagarForm(ModelForm):
+    class Meta:
+        model = Pagar
+        fields = [
+            'tipo_pago',
+            'name_prop',
+            'num_card'
+        ]
+        widgets = {
+            'tipo_pago': forms.RadioSelect(choices=TYPE_PAY,attrs={'class':'form-control mr-3'} ),
+            'name_prop':forms.TextInput(attrs={'class':'form-control mr-3'}),
+            'num_card':forms.TextInput(attrs={'class':'form-control mr-3'})
+        }
--- ticketReservationSystem/pagos/views.py
+++ ticketReservationSystem/pagos/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import PagarForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required(login_url='Login')
+def pagos(request):
+    form = PagarForm()
+    if request.method == 'POST':
+        form = PagarForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {'form':form}
+    return render(request, 'pagos/pagos.html', context)
